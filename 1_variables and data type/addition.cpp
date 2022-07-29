aceacb4e285f2389209a97b83a68305e40aae2f1
Ekam3000 <ekambhelle030@gmail.com>
2022-07-29 23:50:14 +0530

    commit

---
+++ 1_variables and data type/addition.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i = 12;
+	char c = 'a';
+// 'a' is 97 in ascii table 
+	int o = i+ c;
+	char d = i+ c;
+	cout << o << endl;
+	cout << d << endl;
+}
+/*
+109
+m
+*/
